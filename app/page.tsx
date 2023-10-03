c217e1c9e23ed2556579d3924a7727b8bfb9cb90
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-10-03 09:22:57 -0500

    feat(project): :sparkles: add car catalogue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,46 @@
-import { Hero } from '@/components'
+import { CustomFilter, Hero, SearchBar } from '@/components'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter  />
+            <CustomFilter  />
+          </div>
+        </div>
+
+        {/* {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )} */}
+      </div>
     </main>
   )
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { SearchManufacturer } from '.';
+
+const SearchBar = () => {
+
+  const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
+
+  const handleSearch = () => {
+    console.log('searching...')
+  }
+
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManuFacturer}
+        />
+        {/* <SearchButton otherClasses='sm:hidden' /> */}
+      </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        {/* <SearchButton otherClasses='sm:hidden' /> */}
+      </div>
+      {/* <SearchButton otherClasses='max-sm:hidden' /> */}
+    </form>
+  )
+}
+
+export default SearchBar
