9171f9669884376bf91f133b2860bd209c55fffb
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-10-03 18:51:17 -0500

    feat(project): :sparkles: add Search Params logic

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -28,7 +28,7 @@ const SearchManufacturer = ({
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManuFacturer}>
         <div className="relative w-full">
           <Combobox.Button className={"absolute top-[14px]"}>
             <Image
